edae8da71b31d05eff726f669bd9e8d95ca61ac1
aoterinoc <a.oterinoc@alumnos.urjc.es>
2014-10-29 16:55:15 +0100

    comentarios client.py
    otra posible mejora sería añadir a tu código un nuevo parámetro que fuese METODO = sys.argv[3] para que en futuras prácticas cuando tengas que implementar un método que no sea register te valga el mismo código.
    En la línea 23 bastaría con cambiar "REGISTER sip:" por METODO.upper().

--- client.py
+++ client.py
@@ -16,18 +16,19 @@ if len(sys.argv) != 6:
 # Dirección IP del servidor.
 SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
-usuario = sys.argv[4]
-expires = sys.argv[5]
+USUARIO = sys.argv[4]
+EXPIRES = sys.argv[5]
 
 # Contenido que vamos a enviar
-LINE = "REGISTER sip:" + usuario + " SIP/2.0\r\n\r\n"
-LINE1 = "Expires: " + expires + "\r\n\r\n"
+LINE = "REGISTER sip:" + USUARIO + " SIP/2.0\r\n\r\n"
+LINE1 = "Expires: " + EXPIRES + "\r\n\r\n"
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, PORT))
 
+print "Enviando: "
 print LINE
 print LINE1
 my_socket.send(LINE + LINE1 + '\r\n')
